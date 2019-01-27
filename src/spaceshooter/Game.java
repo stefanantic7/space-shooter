4e2816f82a44a6d08e8d013b934bbdc020ce966d
Stefan Antic <stefanantic7@gmail.com>
2019-01-27 14:12:57 +0100

    Removed javafx from imports

--- src/spaceshooter/Game.java
+++ src/spaceshooter/Game.java
@@ -5,7 +5,6 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
 
-import javafx.scene.effect.GaussianBlur;
 import spaceshooter.input.KeyManager;
 import spaceshooter.states.GameState;
 import spaceshooter.states.State;
